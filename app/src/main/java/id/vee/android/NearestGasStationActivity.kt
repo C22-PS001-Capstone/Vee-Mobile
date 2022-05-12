464b137e50f048af0f1d5cb7437c5df9cabcf34b
Reza Juliandri <rezajuliandri20@gmail.com>
2022-05-12 14:53:45 +0700

    [Add] Add some of layout in android application

---
+++ app/src/main/java/id/vee/android/NearestGasStationActivity.kt
@@ -0,0 +1,11 @@
+package id.vee.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NearestGasStationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nearest_gas_station)
+    }
+}
